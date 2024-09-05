b08d94f3abedba068fd249271121f852c4e77156
Michiel <michiel_m@live.nl>
2024-09-05 10:55:38 +0200

    Fixed console colors

--- modules/styling/styling.nix
+++ modules/styling/styling.nix
@@ -23,21 +23,36 @@
     };
 
     console.colors = [
-            "232136"
-            "2a273f"
-            "393552"
-            "6e6a86"
-            "908caa"
-            "e0def4"
-            "e0def4"
-            "56526e"
-            "eb6f92"
-            "f6c177"
-            "ea9a97"
-            "3e8fb0"
-            "9ccfd8"
-            "c4a7e7"
-            "f6c177"
-            "56526e"
+            "232136" #    base00-hex
+
+            "eb6f92" #    red
+            "3e8fb0" #    green
+            "f6c177" #    yellow
+            "9ccfd8" #    blue
+            "c4a7e7" #    magenta
+            "ea9a97" #    cyan
+
+            "e0def4" #    base05-hex
+            "6e6a86" #    base03-hex
+
+            "eb6f92" #    red
+            "3e8fb0" #    green
+            "f6c177" #    yellow
+            "9ccfd8" #    blue
+            "c4a7e7" #    magenta
+            "ea9a97" #    cyan
+
+            "e0def4" #    base06-hex
     ];
 }
+
+
+
+# black = "$overlay"  393552
+# red = "$love"       eb6f92
+# green = "$pine"     3e8fb0
+# yellow = "$gold"    f6c177
+# blue = "$foam"      9ccfd8
+# magenta = "$iris"   c4a7e7
+# cyan = "$rose"      ea9a97
+# white = "$text"     e0def4
