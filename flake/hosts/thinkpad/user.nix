17c9c3a8307f6092f06e31e6252d2e7bdbb35774
MichielMag <michiel_m@live.nl>
2024-10-05 15:10:49 +0200

    Added thinkpad host

---
+++ flake/hosts/thinkpad/user.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
+
+{
+  imports = [ ../../modules/default.nix ];
+  config = {
+    modules = {
+      # gui
+      hyprland.enable = true;
+      rofi.enable = true;
+      styling.enable = true;
+      fuzzel.enable = true;
+      terminal.enable = true;
+
+      # dev
+      vscode.enable = true;
+
+      git = {
+        user = "MichielMag";
+        email = "michiel_m@live.nl";
+        enable = true;
+      };
+
+      # system
+      xdg.enable = true;
+      packages.enable = true;
+      ssh.enable = true;
+
+      # Work
+      work-packages.enable = true;
+      vm-windows.enable = true;
+
+      # Social
+      social.enable = true;
+
+      # Media
+      media.enable = true;
+
+    };
+  };
+}
