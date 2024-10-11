2019d2ea2681f1fc91baa1f3dd438e17e509c09f
MichielMag <michiel_m@live.nl>
2024-10-11 14:55:20 +0200

    Added slack and teams configuration to use --use-gl flag

--- flake/modules/work-packages/default.nix
+++ flake/modules/work-packages/default.nix
@@ -1,16 +1,57 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 with lib;
-let cfg = 
-    config.modules.work-packages;
-    
-in {
-    options.modules.work-packages = { enable = mkEnableOption "work-packages"; };
-    config = mkIf cfg.enable {
+let
+  cfg = config.modules.work-packages;
 
-        home.packages = with pkgs; [
-            slack
-            teams-for-linux
+in
+{
+  options.modules.work-packages = {
+    enable = mkEnableOption "work-packages";
+  };
+  config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [
+      slack
+      teams-for-linux
+    ];
+
+    xdg.desktopEntries = {
+      teams-for-linux = {
+        exec = "teams-for-linux --use-gl=egl %U";
+        icon = "teams-for-linux";
+        name = "Microsoft Teams for Linux";
+        comment = "Unofficial Microsoft Teams client for Linux using Electron";
+        categories = [
+          "Network"
+          "InstantMessaging"
+          "Chat"
         ];
+      };
+      slack = {
+        name = "Slack";
+        comment = "Slack Desktop";
+        genericName = "Slack Client for Linux";
+        exec = "slack --use-gl=egl %U";
+        icon = "slack";
+        type = "Application";
+        startupNotify = true;
+        categories = [
+          "GNOME"
+          "GTK"
+          "Network"
+          "InstantMessaging"
+        ];
+        mimeType = [ "x-scheme-handler/slack" ];
+        settings = {
+          StartupWMClass = "Slack";
+        };
+      };
     };
+  };
 }
