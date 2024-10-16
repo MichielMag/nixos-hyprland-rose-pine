500b86270e9f5c6c41c06fcad9a8b673b6e82f1b
MichielMag <michiel_m@live.nl>
2024-10-16 09:10:15 +0200

    Finished lrun script

--- flake/modules/system/packages/.scripts/lrun.sh
+++ flake/modules/system/packages/.scripts/lrun.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Define the search string if provided
+search_string="$1"
+
 # Split XDG_DATA_DIRS by ':' and iterate through each directory
 IFS=':' read -ra DIRS <<< "$XDG_DATA_DIRS"
 for dir in "${DIRS[@]}"; do
   app_dir="${dir}/applications"
   if [ -d "$app_dir" ]; then
-    find "$app_dir" -name "*.desktop" -print
+    if [ -z "$search_string" ]; then
+      # No search string provided, list all .desktop files
+      find "$app_dir" -name "*.desktop" -print
+    else
+      # Search string provided, filter .desktop files
+      find "$app_dir" -name "*.desktop" -print | grep -i "$search_string"
+    fi
   fi
 done
