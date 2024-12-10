0a3a78546850fde5cf52e76ee4c440490daedfc5
MichielMag <michiel_m@live.nl>
2024-12-10 09:29:21 +0100

    Some additions to shyfox config

--- flake/modules/home/networking/firefox/addons.nix
+++ flake/modules/home/networking/firefox/addons.nix
@@ -14,4 +14,17 @@
       platforms = platforms.all;
     };
   };
+  userchrome-toggle-extended = buildFirefoxXpiAddon rec {
+    pname = "userchrome-toggle-extended";
+    version = "2.0.1";
+    addonId = "userchrome-toggle-extended@n2ezr.ru";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4341014/${pname}-${version}.xpi";
+    sha256 = "7bld876y3h9c27aypls6ahsj8bzm1r0ixyhmadbgh4489lf4nrz";
+    meta = with lib; {
+      homepage = "https://github.com/Naezr/userchrome-toggle-extended-2";
+      description = "This extension allows you to toggle userchrome.css styles on-the-fly with buttons and hotkeys. You'll be able to switch up to six styles";
+      license = licenses.mpl2;
+      platforms = platforms.all;
+    };
+  };
 }
